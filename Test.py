ca510d09d749047b6e04f280d1c248be2e083094
Melis Isakov <melis@Meliss-Air.lan>
2022-02-23 20:54:45 -0500

    Initial commit

---
+++ Test.py
@@ -0,0 +1,16 @@
+fav_color = input('Enter your favourite color? ')
+float_number = float(input('Enter a float: '))
+any_char =  input('Enter any digit: ')
+
+print('Output:')
+print(type(fav_color))
+print(type(float_number))
+print(type(any_char))
+
+result_text=fav_color+str(float_number)+any_char
+
+print('Result: ',result_text)
+
+
+
+
---
+++ calculator.py
@@ -0,0 +1,8 @@
+first_digit = int(input('Enter first digit: '))
+second_digit = int(input('Enter second digit: '))
+
+print('+ result = ',(first_digit+second_digit))
+print('- result = ',(first_digit-second_digit))
+print('* result = ',(first_digit*second_digit))
+
+
